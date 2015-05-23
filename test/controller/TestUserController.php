12fa59ca65048131cd80722e61a830c65193ee0c
aholic <ruochen.xu@gmail.com>
2015-05-23 23:34:35 +0800

    update format

--- test/controller/TestUserController.php
+++ test/controller/TestUserController.php
@@ -7,8 +7,8 @@ include_once dirname(__FILE__) . '/../TestController.php';
 class TestUserController extends TestController {
     public function testGetUserInfoAction() {
         $expectedUserInfo = array(
-            1 => array('userId' => '1', 'userName' => 'ahoLic'),
-            2 => array('userId' => '2', 'userName' => 'kost'),
+            1 => array('userId' => '1', 'userNick' => 'ahoLic'),
+            2 => array('userId' => '2', 'userNick' => 'kost'),
         );
 
         $requestUserID = array(1, 2);
